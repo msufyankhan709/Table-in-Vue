3c2880c9976f74ae5888848ed7cd357f0e232969
M Sufyan Khan <msufyankhan709@gmail.com>
2021-10-10 20:46:48 +0500

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,98 @@
 <template>
-  <v-row class="text-center">
-    <v-col cols="12">
-      <v-img
-        :src="require('../assets/logo.svg')"
-        class="my-3"
-        contain
-        height="200"
-      />
-    </v-col>
-
-    <v-col class="mb-4">
-      <h1 class="display-2 font-weight-bold mb-3">
-        Welcome to Vuetify
-      </h1>
-      <ul v-for="todo in Todos" :key="todo.id">
-        <li>{{ todo }}</li>
-      </ul>
-    </v-col>
-  </v-row>
+  <div>
+    <template>
+      <v-text-field v-model="user" label="Search" class="mx-4"></v-text-field>
+    </template>
+    <template>
+      <v-card-title>Todos</v-card-title>
+      <v-card-text>
+        <v-data-table :headers="headers" :items="Todos" :search="user">
+        </v-data-table>
+      </v-card-text>
+    </template>
+    <template>
+      <v-row justify="center">
+        <v-dialog v-model="dialog" persistent max-width="600px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark v-bind="attrs" v-on="on">
+              Open Dialog
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">User Profile</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                    <v-text-field
+                      label="ID"
+                    ></v-text-field>&nbsp;
+                    <v-text-field
+                      label="User Id"
+                    ></v-text-field>
+                    <v-text-field
+                      label="Title"
+                    ></v-text-field>
+                    <v-text-field label="Description"></v-text-field>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="dialog = false">
+                Close
+              </v-btn>
+              <v-btn color="blue darken-1" text @click="dialog = true">
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
+    </template>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
-import Vue from "vue";
-Vue.use(axios);
-
 export default {
   name: "HelloWorld",
 
   mounted() {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-      this.Todos.push(res.data);
-    });
+    this.loadTodos();
+  },
+  methods: {
+    loadTodos() {
+      fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((response) => response.json())
+        .then((json) => {
+          this.Todos = json;
+        });
+    },
   },
 
   data() {
     return {
       Todos: [],
+      user: "",
+      headers: [
+        {
+          text: "User Id",
+          value: "userId",
+        },
+        {
+          text: "ID",
+          value: "id",
+        },
+        {
+          text: "Title",
+          value: "title",
+        },
+        {
+          text: "Description",
+          value: "completed",
+        },
+      ],
     };
   },
 };
---
+++ src/components/checking.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{message}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'checking',
+
+        data() {
+            return {
+             message:'Hello World',
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
